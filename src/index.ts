77a19cf0eba3ca00d0ef238fb786accc6664103b
Zhibin Cai <yourcaizhibin@gmail.com>
2016-09-17 12:49:16 -0400

    fix(param): Fix the bug that the params are not parsed

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ import { handleRoutesFor } from './routes';
 let server = restify.createServer({
     name: 'TinyUrl App'
 });
-
-// This line MUST appear before any route declaration such as the one below
 // Restify Middleware
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+// This line MUST appear before any route declaration such as the one below
 server.use(restify.throttle({
     burst: 100,
     rate: 50,
     ip: true
 }));
-server.use(restify.bodyParser());
 
 handleRoutesFor(server);
 
--- src/routes.ts
+++ src/routes.ts
@@ -8,20 +8,20 @@ let siteName = "http://api.zhibincai.com" ;
 let respond = (req, res, next) => {
     res.send('hello ' + req.params.name);
     next();
-}
+};
 
 let storeTinyUrl = (req, res, next) => {
     let longUrl = req.params.url;
     save(longUrl).then(data => {
-        let id = data['id']
-        let postfix = idToShortenUrl(id)
+        let id = data['id'];
+        let postfix = idToShortenUrl(id);
         res.send(201, {
             success: true,
             shorten: `${ siteName }/${ postfix }`
         });
     });
     next();
-}
+};
 
 let redirectTo = (req, res, next) => {
     let shortenParam = req.params.shorten;
@@ -35,19 +35,18 @@ let redirectTo = (req, res, next) => {
     .catch(error => {
         res.send(404, `Sorry the url you requested is not found... ${ error }`);
     });
-}
+};
 
 let goToHomePage = (req, res, next) => {
     res.send('home page');
     next();
-}
+};
 
 export let handleRoutesFor = (server: restify.Server) => {
-    // Router
     server.get('/home/:name', respond);
     server.head('/home/:name', respond);
 
     // server.get(':home', goToHomePage);
     server.post('url/create', storeTinyUrl);
     server.get('url/:shorten', redirectTo);
-}
+};
