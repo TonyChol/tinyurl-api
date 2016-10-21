cc56aa2dbd643b782c1b145fa7cf296835de4023
Zhibin Cai <tonycai1992@gmail.com>
2016-10-20 20:47:13 -0400

    feat(random): Apply random tinyurl schema

--- src/helpers/shorten.ts
+++ src/helpers/shorten.ts
@@ -1,24 +1,23 @@
 // Helper functions that supports the functionalities
 // about shorten the urls
 // --------------------------------------------------
-// id: The id of the actual url in the database
 
 let chars: string = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let shuffleChars: string = "wjGJRzNH90ua78gbvB6sICXYpeAWc3ZlryhxOE1kTdtSoP4LMDKQUqF5nVifm2";
 
 let base62 = (character: string) => {
-    return chars.indexOf(character);
+    return shuffleChars.indexOf(character);
 };
 
 export const idToShortenUrl = (id: number) => {
     let shortUrl = "";
     while (id > 0) {
-        shortUrl = chars.charAt(id % 62) + shortUrl;
+        shortUrl = shuffleChars.charAt(id % 62) + shortUrl;
         id = Math.floor(id / 62);
     }
     while (shortUrl.length < 6) {
-        shortUrl = "0" + shortUrl;
+        shortUrl = shuffleChars[0] + shortUrl;
     }
     return shortUrl;
 };
